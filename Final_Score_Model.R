17344b0a388d609ee3c509d6adbe8c69eb3f4526
smazurski <steven.mazurski@gmail.com>
2021-03-12 12:00:36 -0600

    Add files via upload

--- Final_Score_Model.R
+++ Final_Score_Model.R
@@ -20,6 +20,16 @@ Orig_Score_Data <- Orig_Score_Data %>%
   select(-GameID, -Offense, -Defense)
 
 
+# View data distributions
+Orig_Score_Data %>%
+  keep(is.numeric) %>%
+  gather() %>%
+  ggplot() +
+  geom_histogram(mapping = aes(x=value,fill=key), color="black") +
+  facet_wrap(~ key, scales = "free") +
+  theme_minimal()
+
+
 # View Corr plot
 Orig_Score_Data %>%
   keep(is.numeric) %>%
@@ -92,48 +102,62 @@ summary(Orig_Score_Model)
 #Let's test the predictions
 Score_Data <- Orig_Score_Data
 
+
+#Split the dataset
 test_data_train <- data.frame(Score_Data[1:3400,])
 test_data_test <- data.frame(Score_Data[3401:4365,])
 
+# Create Model
 lm_train <- lm(test_data_train$Points~., data = test_data_train)
+
+# Make predictions
 lm_pred <- predict(lm_train, test_data_test)
 
+
+#Convert to dataframe and merge for residuals
 lm_pred <- data.frame(lm_pred)
-
 lm_pred$Points <- test_data_test$Points
-
 lm_pred <- na.omit(lm_pred)
 
+
+#RMSE .7428
 sqrt(mean(lm_pred$lm_pred[1:965]-lm_pred$Points[1:965])^2)
+
 
 lm_pred <- lm_pred %>%
   mutate(diff=lm_pred-Points)
 
 mean(lm_pred$diff)
 median(lm_pred$diff)
-hist(lm_pred$diff)
-sd(lm_pred$diff) # ~68% of predictions will be within 10 points
+# The mean/median predictions distribution are pretty close to zero. Looks like we're slightly
+# underestimating the scores
+
+ggplot(lm_pred, aes(x=diff)) +
+  geom_density(alpha=0.8, color = 'lightblue', fill = 'lightblue') +
+  theme_classic()
 
 
-# Visualize Residuals
-ggplot(data = lm_pred, mapping = aes(x=Points, y=lm_pred )) +
-  geom_point() +
-  geom_smooth(method = "lm")
+sd(lm_pred$diff)
+ # 1 SD = 10.11913 points
 
 
-
+## Let's try a LASSO regression model
 
 #lasso
 library(glmnet)
+
+# Split target variable from features
 x <- model.matrix(Score_Data$Points~.,Score_Data)[,-1]
 y <- Score_Data$Points
 
 lambda <- 10^seq(10, -2, length = 100)
 
+#split test and training datasets
 train=sample(x[1:3400,])
 test=(x[3401:4365,])
 y.test=y[3401:4365]
 
+# Create Model
 lasso.mod=glmnet(x[1:3400,],y[1:3400], alpha=1, lambda = lambda)
 plot(lasso.mod)
 
@@ -142,6 +166,8 @@ cv.out=cv.glmnet(x[1:3400,],y[1:3400],alpha=1)
 plot(cv.out)
 bestlam=cv.out$lambda.min
 lasso.pred=predict(lasso.mod,s=bestlam,newx = x[3401:4365,])
+
+# RMSE: .7227...Slightly lower than linear regression model
 sqrt(mean(lasso.pred-y.test)^2)
 
 
@@ -153,7 +179,8 @@ lasso_data <- lasso_data %>%
 mean(lasso_data$difference)
 median(lasso_data$difference)
 hist(lasso_data$difference)
-sd(lasso_data$difference) # slightly better than the multiple regression model
+sd(lasso_data$difference) 
+# slightly wider distribution than the multiple regression model,  # 1 SD = 10.12273 points
 
 
 out=glmnet(x,y,alpha = 1,lambda = lambda)
